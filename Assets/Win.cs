823381cd66e147df37722409ed317ef0d62cf116
fstleo <fst0leo@gmail.com>
2020-02-01 09:24:51 +0200

    init

---
+++ Assets/Win.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    [SerializeField] private GameObject _text;
+    
+    private void OnMouseDown()
+    {
+        _text.SetActive(true);
+    }
+}
