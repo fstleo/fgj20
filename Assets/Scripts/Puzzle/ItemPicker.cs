53334a794955ef84769377f5d41f78803e3449cb
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2020-02-01 09:37:48 +0200

    Puzzle prototyping

---
+++ Assets/Scripts/Puzzle/ItemPicker.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class ItemPicker : MonoBehaviour
+{
+    private Camera _camera;
+    private PickableItem _pickedItem;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse down");
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, LayerMask.GetMask("PickableItem")))
+            {
+                _pickedItem = hit.transform.GetComponent<PickableItem>();
+                if (_pickedItem != null)
+                {
+                    _pickedItem.OnPicked();
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0) && _pickedItem != null)
+        {
+            Debug.Log("Mouse up");
+            _pickedItem.OnReleased();
+            _pickedItem = null;
+        }
+
+        if (_pickedItem != null && (!Mathf.Approximately(Input.GetAxis("Mouse X"), 0f) || !Mathf.Approximately(Input.GetAxis("Mouse Y"), 0f)))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, LayerMask.GetMask("PickedItemsDragPlane")))
+            {
+                _pickedItem.transform.position = hit.point;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Puzzle/PickableItem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PickableItem : MonoBehaviour
+{
+    private Color _defaultMaterialColor;
+    private Vector3 _initialPosition;
+    
+    private void Awake()
+    {
+        _defaultMaterialColor = GetComponent<Renderer>().material.color;
+        _initialPosition = transform.localPosition;
+    }
+
+    public void OnPicked()
+    {
+        Debug.Log("OnPicked");
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void OnReleased()
+    {
+        Debug.Log("OnReleased");
+        GetComponent<Renderer>().material.color = _defaultMaterialColor;
+    }
+}
