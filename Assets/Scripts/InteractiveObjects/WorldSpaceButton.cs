dc4f77d12a15fc38167dc263d604b354f5ec6255
fstleo <fst0leo@gmail.com>
2020-02-01 10:44:56 +0200

    Button animation

---
+++ Assets/Scripts/InteractiveObjects/WorldSpaceButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldSpaceButton : MonoBehaviour, IInteractive
+{
+    
+    [SerializeField]
+    private string _annotation;
+
+    public string Annotation => _annotation;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    
+    public void Interact()
+    {
+        _animator.SetInteger("Pressed", 1);    
+    }           
+}
