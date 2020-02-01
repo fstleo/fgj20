0bec20fb90f0a86e74b8047042fc34f3bae30abb
fstleo <fst0leo@gmail.com>
2020-02-01 10:11:35 +0200

    Cleanup, fps move

---
+++ Assets/Scripts/Moving/PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private CharacterController _charController;
+
+    [SerializeField] private Transform _cameraTform;
+    [SerializeField] private Vector3 _cameraOffset;
+        
+    [SerializeField] private float _speed = 1f;
+    [SerializeField] private float _rotatingSpeed = 1f;
+    
+    private Transform _transform;    
+    
+    private void Awake()
+    {
+        _transform = transform;
+        _charController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;        
+    }
+    
+    private void Update()
+    {
+        _charController.Move(_speed * _transform.forward * Input.GetAxis("Vertical") * Time.deltaTime);
+        _charController.Move(_speed * _transform.right * Input.GetAxis("Horizontal") * Time.deltaTime);
+              
+
+        _transform.rotation = Quaternion.Euler(_transform.rotation.eulerAngles.x, _transform.rotation.eulerAngles.y + Input.GetAxis("Mouse X") * _rotatingSpeed, transform.rotation.eulerAngles.z);                
+    }
+    
+    void LateUpdate()
+    {                
+        _cameraTform.position = _transform.position + _cameraOffset;
+        var rotationY = _cameraTform.rotation.eulerAngles.x - Input.GetAxis("Mouse Y") * _rotatingSpeed;
+//        rotationY = rotationY > 280 || rotationY < 80 ? rotationY : (rotationY > 180 ? 280 : 80);
+        _cameraTform.rotation = Quaternion.Euler(rotationY, 
+            _transform.rotation.eulerAngles.y, _transform.rotation.eulerAngles.z);
+    }
+}
