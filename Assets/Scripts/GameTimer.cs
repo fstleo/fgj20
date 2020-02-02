1e9776172210392fb333985688abc6c9be0e2f1e
fstleo <fst0leo@gmail.com>
2020-02-02 13:43:39 +0200

    Some fix

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -9,12 +9,21 @@ public class GameTimer : MonoBehaviour
     private float _timer = 60f;
 
     [SerializeField]
-    private TextMeshProUGUI _timerLabel;
+    private TextMeshProUGUI [] _timerLabels;
     
     private void Awake()
     {
         GetComponent<PuzzleRandomizer>().OnPuzzleFix += IncreaseTimer;
-        _timerLabel.text = _timer.ToString("F0");
+
+        ShowTime();   
+    }
+
+    private void ShowTime()
+    {
+        foreach (var timerLabel in _timerLabels)
+        {
+            timerLabel.text = _timer.ToString("F0");
+        }
     }
 
     private void IncreaseTimer()
@@ -25,7 +34,7 @@ public class GameTimer : MonoBehaviour
     private void Update()
     {
         _timer -= Time.deltaTime;
-        _timerLabel.text = _timer.ToString("F0");
+        ShowTime();
         if (_timer < 0)
         {
             SceneManager.LoadScene("FPSTest");
---
+++ Assets/Scripts/WinEffectEgg.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinEffectEgg : MonoBehaviour
+{
+    
+    [SerializeField]
+    private GameObject _normal;
+    
+    [SerializeField]
+    private GameObject _egg;
+    
+    
+    private bool _enabled = false;
+    private float _timer = 10f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 1f) > 0.999f)
+        {
+            _enabled = true;
+            _egg.SetActive(true);
+            _normal.SetActive(false);
+        }
+
+        if (_enabled)
+        {
+            _timer -= Time.deltaTime;
+            if (_timer < 0)
+            {
+                _egg.SetActive(false);
+                _normal.SetActive(true);
+                _timer = 10f;
+                _enabled = false;
+            }                
+        }
+        
+    }
+}
