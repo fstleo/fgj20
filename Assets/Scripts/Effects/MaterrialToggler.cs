efb8ef34d89a2ffe54d34f3603d836a14d3e204e
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2020-02-02 12:42:04 +0200

    Effects

---
+++ Assets/Scripts/Effects/MaterrialToggler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MaterrialToggler : MonoBehaviour
+{
+    [SerializeField]
+    private Material _newMaterial;
+
+    [SerializeField]
+    private int _timeStepMs = 50;
+
+    [SerializeField]
+    private float _probability = 0.2f;
+    
+    private Material _nextMaterial;
+    private Renderer _renderer;
+    private float _nextCheckTime;
+
+    void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+        _nextMaterial = _newMaterial;
+    }
+
+    private void Update()
+    {
+        if (Time.time >= _nextCheckTime)
+        {
+            _nextCheckTime += _timeStepMs / 1000f;
+            if (Random.Range(0f, 1f) <= _probability)
+            {
+                Material m = _renderer.material;
+                _renderer.material = _nextMaterial;
+                _nextMaterial = m;
+            }
+        }
+    }
+}
