1bbf591f0e5b409926fbff73bce365128262902b
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2020-02-02 13:38:41 +0200

    Main menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Button _startGameButton;
+
+    private void Start()
+    {
+        _startGameButton.onClick.AddListener(StartGame);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("FPSTest");
+    }
+}
