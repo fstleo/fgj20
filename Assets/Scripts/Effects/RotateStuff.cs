3c76a207afcdfc9fd10da44849b10eab1382947e
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2020-02-02 11:51:23 +0200

    Visual

---
+++ Assets/Scripts/Effects/RotateStuff.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class RotateStuff : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 30;
+
+    [SerializeField]
+    private Vector3 _axis = Vector3.up;
+
+    [SerializeField]
+    private Transform _targetTransform;
+
+    private void Awake()
+    {
+        if (_targetTransform == null)
+        {
+            _targetTransform = transform;
+        }
+    }
+
+    private void Update()
+    {
+        _targetTransform.Rotate(_axis, _speed * Time.deltaTime);
+    }
+}
