59ff5465febd18dd6d4913caf5149019d14fea9c
fstleo <fst0leo@gmail.com>
2020-02-02 14:05:14 +0200

    Win scene

---
+++ Assets/Scripts/FlyForward.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyForward : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _to;
+
+    private Vector3 _startPosition;
+    [SerializeField]
+    private float _time;
+    
+    private float _timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(_startPosition, _to.position, _timer / _time);
+        _timer += Time.deltaTime;
+    }
+}
