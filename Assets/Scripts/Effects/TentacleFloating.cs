dd0a403ff333f7c46d115b4b65a8e3c57972a3e4
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2020-02-02 13:16:35 +0200

    Tentacles

---
+++ Assets/Scripts/Effects/TentacleFloating.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TentacleFloating : MonoBehaviour
+{
+    private float _floatingStartTime;
+
+    private Vector3 _floatingCenter;
+    
+    private void Start()
+    {
+        _floatingCenter = transform.localPosition;
+        _floatingStartTime = Random.Range(0f, 500f);
+    }
+    
+    private void Update()
+    {
+        transform.localPosition = _floatingCenter + 0.05f * Mathf.Sin((Time.time - _floatingStartTime) / 2f) * Vector3.up;
+    }
+}
