c3e613c1a2fd03fc479fba5dc20c2ac2469d7fb0
fstleo <fst0leo@gmail.com>
2020-02-02 14:16:18 +0200

    Lose/win scene

--- Assets/Scripts/FlyForward.cs
+++ Assets/Scripts/FlyForward.cs
@@ -12,16 +12,27 @@ public class FlyForward : MonoBehaviour
     private float _time;
     
     private float _timer;
+
+    private Vector3 _direction;
     // Start is called before the first frame update
     void Start()
     {
+        _direction = Random.onUnitSphere;
         _startPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(_startPosition, _to.position, _timer / _time);
-        _timer += Time.deltaTime;
+        if (_to == null)
+        {
+            transform.position +=  _direction * 5 * Time.deltaTime;
+            transform.rotation *= Quaternion.Euler(Random.Range(0,5f),Random.Range(0,5f),Random.Range(0,5f));
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(_startPosition, _to.position, _timer / _time);
+            _timer += Time.deltaTime;
+        }
     }
 }
