a34a92c1047ce9324fd9e6628351a4f1d5d6fad7
fstleo <fst0leo@gmail.com>
2020-02-02 10:52:24 +0200

    Brain scaling

---
+++ Assets/Scripts/ScaleStuff.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleStuff : MonoBehaviour
+{
+    private float _startScale;
+
+    private void Awake()
+    {
+        _startScale = transform.localScale.x;
+    }
+    
+    private void Update()
+    {
+        float fx = _startScale + Mathf.Sin(Time.time)/20f;
+        transform.localScale = new Vector3(fx, fx, fx);
+    }
+}
